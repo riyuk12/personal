292d1481c3800c51347bcb7c31d1aeb2b602ea7a
riyuk12 <rajatbose2641@gmail.com>
2023-08-15 13:36:55 +0530

    auth base page done

--- filter/src/pages/authentication/AuthPage.jsx
+++ filter/src/pages/authentication/AuthPage.jsx
@@ -4,10 +4,15 @@ import Login from '../../components/Authentication/Login'
 import Register from '../../components/Authentication/Register'
 import './Auth.css'
 
+import filter from '../../assets/filter.png'
+
 const BaseCard=()=>{
     return(
         <div className='cardContainer'>
-            <div className="card card1"><Outlet></Outlet></div>
+            <div className="card card1">
+              <img className='logo' src={filter}  />
+              <Outlet></Outlet>
+            </div>
             <div className="card card2"></div>
             <div className="card card3"></div>
         </div>
@@ -15,13 +20,45 @@ const BaseCard=()=>{
 }
 
 function AuthPage() {
+  let x=0;
+  let y=0;
+  document.onmousemove = function(event)
+  {
+    x = event.pageX;
+    y = event.pageY;
+    
+    var root = document.querySelector(':root');
+    var rootStyles = getComputedStyle(root);
+    var offset= rootStyles.getPropertyValue('--offset');
+    var offset2= rootStyles.getPropertyValue('--offset2');
+    var offset3= rootStyles.getPropertyValue('--offset3');
+
+    let width=window.innerWidth
+    let height=window.innerHeight
+
+    root.style.setProperty('--positionX', `${(((x/width)-0.5)*Number(offset))+(width/2)-200}px`)
+    root.style.setProperty('--positionY', `${(((y/height)-0.5)*Number(offset))+(height/2)-275}px`)
+    root.style.setProperty('--positionX2', `${(((x/width)-0.5)*Number(offset2))+(width/2)-200}px`)
+    root.style.setProperty('--positionY2', `${(((y/height)-0.5)*Number(offset2))+(height/2)-275}px`)
+    root.style.setProperty('--positionX3', `${(((x/width)-0.5)*Number(offset3))+(width/2)-200}px`)
+    root.style.setProperty('--positionY3', `${(((y/height)-0.5)*Number(offset3))+(height/2)-275}px`)
+    root.style.setProperty('--positionXbg', `${-(((x/width)-0.5)*Number(offset2))}px`)
+    root.style.setProperty('--positionYbg', `${-(((y/height)-0.5)*Number(offset2))}px`)
+
+  }
+  
+   
   return (
-    <Routes>  
-      <Route path='/' element={<BaseCard />}>
-        <Route path='/' element={ <Login/> }/>
-        <Route path='/register' element={<Register/>}/>
-      </Route>
-    </Routes>
+    <div className='container'>
+      <div className='dots'>
+      </div>
+      <Routes>  
+        <Route path='/' element={<BaseCard />}>
+          <Route path='/' element={ <Login/> }/>
+          <Route path='/register' element={<Register/>}/>
+        </Route>
+      </Routes>
+    </div>
   )
 }
 
