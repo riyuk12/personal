cb2adf78b488225752ecbc1ea3843ad7e6c8d8bc
Rajat Bose <rajatbose2641@gmail.com>
2023-08-27 12:38:48 +0530

    popup for explore cards

--- filter/src/components/dashboard/ExploreCards.jsx
+++ filter/src/components/dashboard/ExploreCards.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import { FavoriteRounded } from '@mui/icons-material'
+import { useState } from 'react'
+import ModalPostViewer from './ModalPostViewer';
 
 function ExploreCards({post}) {
+  const [open,setOpen]=useState(false);
+
+  const handleOpen=()=>{
+    setOpen(true)
+    console.log("settrue")
+  }
+  const handleClose=()=>{
+    setOpen(false)
+    console.log("setfalse")
+  }
+  
+
   return (
-    <div className='explore_post_container'>
-        <div className="imagebase">
+    <div className='explore_post_container' >
+      <ModalPostViewer open={open} onClose={handleClose} post={post}/>
+        <div className="imagebase" onClick={handleOpen}>
             <img src={post?.image_url}  />
             <div className="card_cover">
                 <FavoriteRounded />
