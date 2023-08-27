82a8388401039a0f1cc194ebd1f79493537c628e
Rajat Bose <rajatbose2641@gmail.com>
2023-08-27 12:38:15 +0530

    popup for post grid click

---
+++ filter/src/components/dashboard/ModalPostViewer.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Modal ,Button } from '@mui/material'
+import { useState } from 'react'
+import Post from './Post'
+
+
+
+function ModalPostViewer({open,onClose,post}) {
+
+  return (
+    <Modal
+      open={open}
+      onClose={onClose}
+      aria-labelledby="modal-title"
+      aria-describedby="modal-description"
+
+      sx={{'& .MuiModal-backdrop':{
+        display:'flex' ,
+        justifyContent:'center',
+        alignItems:'center'
+      }}}
+    >
+      <div className="modal-content">
+        <Button onClick={onClose}>Close Modal</Button>
+        <Post  imgurl={post?.image_url} desc={ post?.description} username={post?.username} profile_picture={post?.profile_picture_url} />
+      </div>
+    </Modal>
+  )
+}
+
+export default ModalPostViewer
