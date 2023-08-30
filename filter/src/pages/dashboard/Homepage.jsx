9176e5855d70820ddeacf485adeeaecd3f9590f3
Rajat Bose <rajatbose2641@gmail.com>
2023-08-30 21:29:48 +0530

    ok

--- filter/src/pages/dashboard/Homepage.jsx
+++ filter/src/pages/dashboard/Homepage.jsx
@@ -13,7 +13,7 @@ function Homepage() {
     const [posts,setposts]=useState([])
     const [activetab,setactivetab]=useState('home'); 
     const [{user},dispatch]=useDataLayerValue()
-    const [activeProfile,setactiveProfile]=useState(user) 
+    const [data,setData]=useState([]) 
     const [userdata,setuserdata]=useState([]) 
 
     useEffect(()=>{
@@ -32,8 +32,6 @@ function Homepage() {
         })
         .then((data)=>setuserdata(data))
         .catch((error)=>{console.log(error)})
-
-        setuserdata(userdata.filter((elem)=>elem.basicProfile.username!=user.basicProfile.username))
     },[])
 
 
@@ -47,32 +45,45 @@ function Homepage() {
                 <div className="tab home" onClick={()=>setactivetab("home")}> home</div>
                 <div className="tab explore" onClick={()=>setactivetab("explore")}>explore</div>
                 {/* <div className="tab create" onClick={()=>setactivetab("create")}>create</div> */}
-                <div className="tab profile" onClick={()=>{setactivetab("profile")}}>profile</div>
+                <div className="tab profile" onClick={()=>setactivetab("profile")}>profile</div>
             </div>
 
         </div>
         <div className="main_container">
             {(activetab=="home") && <Feed posts={posts}/>}
             {(activetab=="explore") && <Explore posts={posts} />}
-            {(activetab=="profile") && <UserProfile user={activeProfile } posts={posts} />}
+            {(activetab=="profile") && <UserProfile user={user } posts={posts} />}
 
             {/* could use router to make better look it up */}
         </div>
         <div className="following">
-            <h1>Suggested Users</h1>
+            <div className="profile_link">
+                <div className="user user1" onClick={()=>{setactivetab("profile")}}>
+                    <img src={filter} />
+                    <h2>riyuk</h2>
+                </div>
+            </div>
             <div className="suggestedusers">
-                
-                {userdata.map((elem)=>(
-                    <div className="user user1" 
-                    onClick={()=>{
-                        setactivetab("profile")
-                        setactiveProfile(elem)
-                        }}  >
-                        <img src={filter} />
-                        <h2>{elem.basicProfile.username}</h2>
-                    </div>
-                ))}
-                
+                <div className="user user1"  >
+                    <img src={filter} />
+                    <h2>user1</h2>
+                </div>
+                <div className="user user2">
+                    <img src="" />
+                    <h2>user2</h2>
+                </div>
+                <div className="user user3">
+                    <img src="" />
+                    <h2>user3</h2>
+                </div>
+                <div className="user user4">
+                    <img src="" />
+                    <h2>user4</h2>
+                </div>
+                <div className="user user5">
+                    <img src="" />
+                    <h2>user5</h2>
+                </div>
             </div>
         </div>
     </div>
